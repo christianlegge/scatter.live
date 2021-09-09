d5457615ccbe6de1ea3909c9236e04f865e0451c
scatter-dev <christianlegge888@gmail.com>
2021-09-08 21:16:06 -0400

    save as txt

--- public/javascripts/wordlengths.js
+++ public/javascripts/wordlengths.js
@@ -2,110 +2,116 @@ var keypressed = false;
 var saveTimeout = null;
 
 function focusText() {
-  keypressed = true;
-  var el = document.getElementById("mainText");
-  if (document.activeElement !== el) {
-    el.focus();
-    window.getSelection().selectAllChildren(el);
-    window.getSelection().collapseToEnd();
-  }
+	keypressed = true;
+	var el = document.getElementById("mainText");
+	if (document.activeElement !== el) {
+		el.focus();
+		window.getSelection().selectAllChildren(el);
+		window.getSelection().collapseToEnd();
+	}
 }
 
 var app = angular.module('wordlengths', ['ngSanitize']);
 
 app.directive('contenteditable', ['$sce', function ($sce) {
-    return {
-      restrict: 'A', // only activate on element attribute
-      require: '?ngModel', // get a hold of NgModelController
-      link: function (scope, element, attrs, ngModel) {
-        if (!ngModel) return; // do nothing if no ng-model
+		return {
+			restrict: 'A', // only activate on element attribute
+			require: '?ngModel', // get a hold of NgModelController
+			link: function (scope, element, attrs, ngModel) {
+				if (!ngModel) return; // do nothing if no ng-model
 
-        // Specify how UI should be updated
-        ngModel.$render = function () {
-          element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
-        };
+				// Specify how UI should be updated
+				ngModel.$render = function () {
+					element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
+				};
 
-        // Listen for change events to enable binding
-        element.on('blur keyup change', function () {
-          scope.$evalAsync(read);
-        });
-        read(); // initialize
+				// Listen for change events to enable binding
+				element.on('blur keyup change', function () {
+					scope.$evalAsync(read);
+				});
+				read(); // initialize
 
-        // Write data to the model
-        function read() {
-          var html = element.html();
-          // When we clear the content editable the browser leaves a <br> behind
-          // If strip-br attribute is provided then we strip this out
-          if (attrs.stripBr && html == '<br>') {
-            html = '';
-          }
-          ngModel.$setViewValue(html);
-        }
-      }
-    };
-  }]);
+				// Write data to the model
+				function read() {
+					var html = element.html();
+					// When we clear the content editable the browser leaves a <br> behind
+					// If strip-br attribute is provided then we strip this out
+					if (attrs.stripBr && html == '<br>') {
+						html = '';
+					}
+					ngModel.$setViewValue(html);
+				}
+			}
+		};
+	}]);
+
+function innerTextsFromMainText(mainText) {
+	var el = document.createElement('html');
+	el.innerHTML = mainText;
+	return Array.from(el.getElementsByTagName('div')).map(x => x.innerText);
+}
 
 app.controller('wordlengthsController', function($scope) {
+	$scope.mainText = "";
+	$scope.lineLengths = [];
+	$scope.dom = null;
 
-    $scope.mainText = "";
-    $scope.lineLengths = [];
-    $scope.dom = null;
+	$scope.mainTextChanged = function() {
+		$scope.lineLengths = innerTextsFromMainText($scope.mainText).map(x => x.replace(/\W/g, '')).map(x => x.length);
 
-    $scope.mainTextChanged = function() {
-      var el = document.createElement('html');
-      el.innerHTML = $scope.mainText;
-      var lines = el.getElementsByTagName("div");
+		if (keypressed) {
+			clearTimeout(saveTimeout);
+			saveTimeout = setTimeout(function() { 
+				localforage.setItem("mainText", $scope.mainText, function(err) {
+					if (err == null) {
+						var el = document.getElementById("saved");
+						el.classList.add("saved-anim");
+						el.style.animation = 'none';
+						el.offsetHeight;
+						el.style.animation = null;
+						
+					}
+					else {
+						console.log(err);
+					}
+				});
+			}, 5000);
+		}
+	};
 
-      $scope.lineLengths = Array.from(lines).map(x => x.innerText).map(x => x.replace(/\W/g, '')).map(x => x.length);
-      $scope.lineLengthsString = $scope.lineLengths.join("\n");
+	$scope.saveAsTxt = function() {
+		var lines = innerTextsFromMainText($scope.mainText);
+		var linesWithLength = [];
+		for (var i = 0; i < lines.length; i++) {
+			linesWithLength.push(lines[i] + ($scope.lineLengths[i] > 0 ? (" (" + $scope.lineLengths[i] + ")") : ""));
+		}
+		var blob = new Blob([linesWithLength.join("\r\n")], { type: "text/plain;charset=utf-8" });
+		window.saveAs(blob, "words.txt");
+	};
 
-      console.log($scope.lineLengths);
-      console.log($scope.mainText);
-
-      if (keypressed) {
-        clearTimeout(saveTimeout);
-        saveTimeout = setTimeout(function() { 
-          localforage.setItem("mainText", $scope.mainText, function(err) {
-            if (err == null) {
-              var el = document.getElementById("saved");
-              el.classList.add("saved-anim");
-              el.style.animation = 'none';
-              el.offsetHeight;
-              el.style.animation = null;
-              
-            }
-            else {
-              console.log(err);
-            }
-          });
-        }, 5000);
-      }
-    };
-
-    localforage.getItem("mainText", function(err, value) {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        if (value == null) {
-          $scope.mainText = "<div>Type here...</div>"
-        }
-        else {
-          $scope.mainText = value;
-        }
-        $scope.mainTextChanged();
-        $scope.$apply();
-      }
-    })
-  
+	localforage.getItem("mainText", function(err, value) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			if (value == null) {
+				$scope.mainText = "<div>Type here...</div>"
+			}
+			else {
+				$scope.mainText = value;
+			}
+			$scope.mainTextChanged();
+			$scope.$apply();
+		}
+	})
 });
 
 app.filter('range', function(){
-    return function(n) {
-      var res = [];
-      for (var i = 0; i < n; i++) {
-        res.push(i);
-      }
-      return res;
-    };
-  });
+		return function(n) {
+			var res = [];
+			for (var i = 0; i < n; i++) {
+				res.push(i);
+			}
+			return res;
+		};
+	});
